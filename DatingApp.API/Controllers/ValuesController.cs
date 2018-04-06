bf8fe977264c52ca6348e909c96859072ca856ac
Alex Alexis <alxalexis@outlook.com>
2018-04-06 12:28:36 -0400

    Starting to think in terms of Async methods

--- DatingApp.API/Controllers/ValuesController.cs
+++ DatingApp.API/Controllers/ValuesController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using DatingApp.API.Data;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace DatingApp.API.Controllers
 {
@@ -13,19 +14,27 @@ namespace DatingApp.API.Controllers
         private readonly DataContext _context;
         public ValuesController(DataContext context) => _context = context;
         // GET api/values
-        [HttpGet]
-        public IActionResult GetValues()
-        {
-            var values = _context.Values.ToList();
+        // [HttpGet]
+        // public IActionResult GetValues()
+        // {
+        //     var values = _context.Values.ToList();
 
+        //     return Ok(values);
+        // }
+        
+        // GET api/values
+        [HttpGet]
+        public async Task<IActionResult> GetValues()
+        {
+            var values = await _context.Values.ToListAsync();
             return Ok(values);
         }
 
         // GET api/values/5
         [HttpGet("{id}")]
-        public IActionResult GetValue(int id)
+        public async Task<IActionResult> GetValue(int id)
         {
-            var value = _context.Values.FirstOrDefault(x => x.Id == id);
+            var value = await _context.Values.FirstOrDefaultAsync(x => x.Id == id);
             return Ok(value);
         }
 
