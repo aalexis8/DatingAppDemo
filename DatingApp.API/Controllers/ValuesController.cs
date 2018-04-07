b8addc8d041bffda54a79dfb0064011f726f16c6
Alex Alexis <alxalexis@outlook.com>
2018-04-07 13:03:33 -0400

    Added Authentication and Authorization

--- DatingApp.API/Controllers/ValuesController.cs
+++ DatingApp.API/Controllers/ValuesController.cs
@@ -3,26 +3,19 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using DatingApp.API.Data;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
 namespace DatingApp.API.Controllers
 {
+    [Authorize]
     [Route("api/[controller]")]
     public class ValuesController : Controller
     {
         private readonly DataContext _context;
         public ValuesController(DataContext context) => _context = context;
-        // GET api/values
-        // [HttpGet]
-        // public IActionResult GetValues()
-        // {
-        //     var values = _context.Values.ToList();
 
-        //     return Ok(values);
-        // }
-        
-        // GET api/values
         [HttpGet]
         public async Task<IActionResult> GetValues()
         {
